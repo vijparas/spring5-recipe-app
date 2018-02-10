8ab6c3f8cddfa49ad4e709b84a2b88af2da08dd9
Paras <vijparas@gmail.com>
2018-02-10 19:17:24 +0530

    Integration testing

---
+++ src/test/java/com/paras/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,24 @@
+package com.paras.repositories;
+
+import com.paras.domain.Recipe;
+import com.paras.domain.UnitOfMeasure;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.Optional;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+    @Test
+    public void findByDescription() {
+        Optional<UnitOfMeasure> uom=unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon",uom.get().getDescription());
+    }
+}
