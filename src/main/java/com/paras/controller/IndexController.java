f7d1982a58bda79ac9565879f23c44acc9e724b9
Paras <vijparas@gmail.com>
2018-02-10 11:55:22 +0530

    Junit test

--- src/main/java/com/paras/controller/IndexController.java
+++ src/main/java/com/paras/controller/IndexController.java
@@ -1,34 +1,33 @@
-package com.paras.controller;
+    package com.paras.controller;
 
-import com.paras.domain.Category;
-import com.paras.domain.Recipe;
-import com.paras.domain.UnitOfMeasure;
-import com.paras.repositories.CategoryRepository;
-import com.paras.repositories.UnitOfMeasureRepository;
-import com.paras.services.RecipeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+    import com.paras.domain.Category;
+    import com.paras.domain.Recipe;
+    import com.paras.domain.UnitOfMeasure;
+    import com.paras.repositories.CategoryRepository;
+    import com.paras.repositories.UnitOfMeasureRepository;
+    import com.paras.services.RecipeService;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
-import java.util.Set;
+    import java.util.Optional;
+    import java.util.Set;
 
-@Controller
+    @Controller
+    public class IndexController {
+        @Autowired
+        private CategoryRepository categoryRepository;
+        @Autowired
+        private UnitOfMeasureRepository unitOfMeasureRepository;
+        @Autowired
+        private RecipeService recipeService;
 
+        @RequestMapping({"/","index"})
+        public String index(Model model){
+            Set<Recipe> recipeSet=recipeService.getRecipes();
+            model.addAttribute("recipes",recipeSet);
+            return "index";
 
-public class IndexController {
-    @Autowired
-    private CategoryRepository categoryRepository;
-    @Autowired
-    private UnitOfMeasureRepository unitOfMeasureRepository;
-    @Autowired
-    private RecipeService recipeService;
-@RequestMapping({"/","index"})
-    public String index(Model model){
-        Set<Recipe> recipeSet=recipeService.getRecipes();
-        model.addAttribute("recipes",recipeSet);
-        return "index";
-
-}
-}
+    }
+    }
